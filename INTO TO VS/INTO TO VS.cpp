6b80deb15cedef0659b72ddfcf147bdaa1ed18e9
m2300041 <m2300041@D1006Z-011C>
2023-11-06 11:38:22 +0000

    Add project files.

---
+++ INTO TO VS/INTO TO VS.cpp
@@ -0,0 +1,43 @@
+// INTO TO VS.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cout << "hello world!\n";
+		int num;
+		cin >> num;
+
+
+
+
+
+	if (num > 0)
+	{
+		cout << "Num is Greater than 0";
+
+	}
+
+
+	else if (num < 0)
+	{
+		cout << "Num is less than 0";
+	}
+	else {
+		cout << "Num is 0 \n";
+		cin >> num;
+	}
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
